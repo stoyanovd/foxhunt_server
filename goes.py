b6ce008878bbf4e7458a2068ee6955f52d5e0b4b
Dmitry Stoyanov <stoyanovd@mail.ru>
2017-11-17 01:35:26 +0300

    Try more cors

--- goes.py
+++ goes.py
@@ -2,17 +2,26 @@ from flask import Flask
 import flask_restful as restful
 import os
 
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = restful.Api(app)
 
+# @app.route("/")
+# @cross_origin()
+# def helloWorld():
+#   return {'hello': 'world'}
+
 class HelloWorld(restful.Resource):
+    @cross_origin()
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+# if __name__ == '__main__':
+#     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+
+app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
