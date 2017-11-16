6521d4060dc208bdf1e623ec61d9a91069c44b07
Dmitry Stoyanov <stoyanovd@mail.ru>
2017-11-17 00:39:58 +0300

    add heroku port

--- goes.py
+++ goes.py
@@ -11,4 +11,4 @@ class HelloWorld(restful.Resource):
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7777)
+    app.run(debug=True, port=os.environ.get('PORT', 5000))
