8218bb8b3c8ac78af082c440bfcc739d6e64f6c3
Dmitry Stoyanov <stoyanovd@mail.ru>
2017-11-17 01:39:13 +0300

    try with adding host

--- goes.py
+++ goes.py
@@ -24,4 +24,4 @@ api.add_resource(HelloWorld, '/')
 # if __name__ == '__main__':
 #     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
 
-app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
